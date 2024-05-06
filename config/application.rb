8af335e662d91fcd296c938dadaea30c81442cb4
Binh Nguyen Dang <binhnguyendang2003@gmail.com>
2024-05-06 17:51:13 -0400

    test4

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module Turbochat
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    
     config.load_defaults 7.1
 
     config.action_view.sanitized_allowed_tags = Loofah::HTML5::SafeList::ALLOWED_ELEMENTS
